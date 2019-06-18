84a4b842b7ed5fa27e6f10a6a44e1a92c9dac059
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-06-18 14:37:37 -0300

    CRUD Queries
    4 products, cargos and types

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,14 +1,34 @@
 const knex = require('../config/server')
 
+const simpleSelect = (tableName, columns = '*') => knex.select(columns).from(tableName)
+
+const simpleInsert = (tableName, data) => knex.insert(data).into(tableName)
+
+const simpleDelete = (tableName, where) => knex(tableName).where(where).del()
+
+const simpleUpdate = (tableName, where, data) => knex(tableName).where(where).update(data)
+
 const queries = {
   select: {
-    products: () => knex.select('*').from('produtos')
+    tipos: () => simpleSelect('tipos_produto'),
+    products: () => simpleSelect('produtos'),
+    cargos: () => simpleSelect('cargos')
   },
   insert: {
-    products: (data) => knex.insert(data).into('produtos')
+    products: data => simpleInsert('produtos', data),
+    cargos: data => simpleInsert('cargos', data),
+    tipos: data => simpleInsert('tipos_produto', data)
   },
-  delete: null,
-  update: null
+  del: {
+    tipos: where => simpleDelete('tipos_produto', where),
+    products: where => simpleDelete('produtos', where),
+    cargos: where => simpleDelete('cargos', where)
+  },
+  update: {
+    tipos: (data, where) => simpleUpdate('tipos_produto', where, data),
+    products: (data, where) => simpleUpdate('produtos', where, data),
+    cargos: (data, where) => simpleUpdate('cargos', where, data)
+  }
 }
 
 module.exports = queries
